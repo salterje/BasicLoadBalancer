0ac2afdde27b3a639db2e9efb48c34939e672b17
salterje <salter.je@gmail.com>
2020-09-15 13:56:52 +0100

    Small tweaks to tidy up scripts

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -1,24 +1,22 @@
 echo "Task 1: Perform YUM update"
-
 yum -y update
+
 echo "Task 2: Add epel-release"
 yum -y install epel-release
-echo "Task 3: Install nginx"
 
+echo "Task 3: Install nginx"
 yum -y install nginx > /dev/null 2>&1
 
 echo "Taks 4: Modify index.html test page"
 sudo cp /home/vagrant/index.html /usr/share/nginx/html/index.html
 
 echo "Task 5: Enable NGINX"
-
 sudo systemctl enable nginx
 
 echo "Task 6: Start NGINX"
-
 sudo systemctl start nginx
 
-echo "Task 9 Update /etc/hosts file"
+echo "Task 7 Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
 192.168.200.201 ha-loadbalancer
 192.168.200.210 nginx-server-1
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,8 @@
 echo "Task 1: Perform YUM update"
-
 yum -y update
 
 echo "Task 2: Install haproxy"
-
 yum -y install haproxy > /dev/null 2>&1
-
-#echo "Task 3: Set SELinux to permissive (For Lab Work Only!)"
-#sudo setenforce permissive
 
 echo "Task 4: Backup haproxy.cfg"
 if [ -f /etc/haproxy/haproxy.cfg.orig ]; then
@@ -22,16 +17,15 @@ sudo chown root:root /home/vagrant/haproxy.cfg
 
 echo "Task 6: Move File from Home to /etc"
 sudo mv /home/vagrant/haproxy.cfg /etc/haproxy/haproxy.cfg
-#sudo chown root:root /etc/haproxy/haproxy.cfg
 
 echo "Task 7: Change SELinux File Type"
 sudo chcon -t etc_t /etc/haproxy/haproxy.cfg 
 
-echo "Task 7: Start HAProxy"
+echo "Task 8: Start HAProxy"
 sudo systemctl start haproxy
 sudo systemctl enable haproxy
 
-echo "Task 8 Update /etc/hosts file"
+echo "Task 9 Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
 192.168.200.201 ha-loadbalancer
 192.168.200.210 nginx-server-1
